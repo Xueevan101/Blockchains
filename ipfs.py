3ad294fe4feed4c07408422794b375e0d81f82cd
Xueevan101 <145930908+Xueevan101@users.noreply.github.com>
2025-06-30 14:29:59 -0400

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -30,7 +30,7 @@ def get_from_ipfs(cid,content_type="json"):
 	#YOUR CODE HERE	
 	if cid.startswith("http"):
         	cid = cid.split("/")[-1]
-	url = f"https://cloudflare-ipfs.com/ipfs/{cid}"
+	url = f"https://ipfs.io/ipfs/{cid}"
 	response = requests.get(url)
 
 	if response.ok:
